38ae2dd9786e63f7ada59450cbdf7b218275e42e
sagar619619 <sc927818@gmail.com>
2024-05-04 12:25:22 +0530

    design newsroom page

---
+++ src/assets/js/newsroom.js
@@ -0,0 +1,17 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.tab button.active').click();
+});
