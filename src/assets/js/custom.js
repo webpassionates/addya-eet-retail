f139a5f5f7a9969d5416f34722abebff33e4a937
sagar619619 <sc927818@gmail.com>
2024-05-08 18:21:44 +0530

    add search button

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,4 +1,31 @@
-$(document).ready(function() {
-    // jQuery code here
-    // alert("Hello EET");
-  });
+// $(document).ready(function() {
+//     // jQuery code here
+//     // alert("Hello EET");
+//   });
+
+searchIcon.addEventListener('mouseenter', expandSearchInput);
+    searchInput.addEventListener('mouseenter', expandSearchInput);
+    function expandSearchInput() {
+        if (!searchInput.classList.contains('expanded')) {
+            searchInput.classList.add('expanded');
+            speakToUsBtn.style.opacity = '0';
+            speakToUsBtn.style.display = 'inline-block';
+            glass.style.color = "white";
+            // searchInput.style.background = '#858C91';
+            // searchInput.style.outline = '2px black solid'
+            // searchInput.style.borderRadius = "10px"
+            searchInput.focus();
+        }
+    }
+    searchIcon.addEventListener('mouseleave', collapseSearchInput);
+    searchInput.addEventListener('mouseleave', collapseSearchInput);
+    function collapseSearchInput() {
+        if (searchInput.classList.contains('expanded')) {
+            searchInput.classList.remove('expanded');
+            speakToUsBtn.style.display = 'inline-block';
+            searchInput.style.background = 'transparent';
+            searchInput.style.outline = 'none'
+            speakToUsBtn.style.opacity = '1';
+            glass.style.color = "black";
+        }
+    }
