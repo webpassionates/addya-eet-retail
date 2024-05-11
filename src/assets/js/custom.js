1309f7333296d4bf10de110b56ff6a1f7f5a0338
sagar619619 <sc927818@gmail.com>
2024-05-11 10:03:55 +0530

    working on corrections

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,30 +2,42 @@
 //     // jQuery code here
 //     // alert("Hello EET");
 //   });
-
+//header search input start//
 searchIcon.addEventListener('mouseenter', expandSearchInput);
-    searchInput.addEventListener('mouseenter', expandSearchInput);
-    function expandSearchInput() {
-        if (!searchInput.classList.contains('expanded')) {
-            searchInput.classList.add('expanded');
-            speakToUsBtn.style.opacity = '0';
-            speakToUsBtn.style.display = 'inline-block';
-            glass.style.color = "white";
-            // searchInput.style.background = '#858C91';
-            // searchInput.style.outline = '2px black solid'
-            // searchInput.style.borderRadius = "10px"
-            searchInput.focus();
-        }
+searchInput.addEventListener('mouseenter', expandSearchInput);
+function expandSearchInput() {
+    if (!searchInput.classList.contains('expanded')) {
+        searchInput.classList.add('expanded');
+        speakToUsBtn.style.opacity = '0';
+        speakToUsBtn.style.display = 'inline-block';
+        glass.style.color = "white";
+        // searchInput.style.background = '#858C91';
+        // searchInput.style.outline = '2px black solid'
+        // searchInput.style.borderRadius = "10px"
+        searchInput.focus();
     }
-    searchIcon.addEventListener('mouseleave', collapseSearchInput);
-    searchInput.addEventListener('mouseleave', collapseSearchInput);
-    function collapseSearchInput() {
-        if (searchInput.classList.contains('expanded')) {
-            searchInput.classList.remove('expanded');
-            speakToUsBtn.style.display = 'inline-block';
-            searchInput.style.background = 'transparent';
-            searchInput.style.outline = 'none'
-            speakToUsBtn.style.opacity = '1';
-            glass.style.color = "black";
-        }
+}
+searchIcon.addEventListener('mouseleave', collapseSearchInput);
+searchInput.addEventListener('mouseleave', collapseSearchInput);
+function collapseSearchInput() {
+    if (searchInput.classList.contains('expanded')) {
+        searchInput.classList.remove('expanded');
+        speakToUsBtn.style.display = 'inline-block';
+        searchInput.style.background = 'transparent';
+        searchInput.style.outline = 'none'
+        speakToUsBtn.style.opacity = '1';
+        glass.style.color = "black";
     }
+}
+
+//header search input end//
+
+//header dropdown mouse hover start//
+function openDropdown() {
+    document.getElementById("dropdownMenu").classList.add("show");
+}
+
+function closeDropdown() {
+    document.getElementById("dropdownMenu").classList.remove("show");
+}
+//header dropdown mouse hover end//
