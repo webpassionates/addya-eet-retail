dd26642c5b160215f3fbcd29fb058f49fa259eb7
sagar619619 <sc927818@gmail.com>
2024-05-14 15:51:06 +0530

    working on corrections

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,33 +2,7 @@
 //     // jQuery code here
 //     // alert("Hello EET");
 //   });
-//header search input start//
-searchIcon.addEventListener('mouseenter', expandSearchInput);
-searchInput.addEventListener('mouseenter', expandSearchInput);
-function expandSearchInput() {
-    if (!searchInput.classList.contains('expanded')) {
-        searchInput.classList.add('expanded');
-        speakToUsBtn.style.opacity = '0';
-        speakToUsBtn.style.display = 'inline-block';
-        glass.style.color = "white";
-        // searchInput.style.background = '#858C91';
-        // searchInput.style.outline = '2px black solid'
-        // searchInput.style.borderRadius = "10px"
-        searchInput.focus();
-    }
-}
-searchIcon.addEventListener('mouseleave', collapseSearchInput);
-searchInput.addEventListener('mouseleave', collapseSearchInput);
-function collapseSearchInput() {
-    if (searchInput.classList.contains('expanded')) {
-        searchInput.classList.remove('expanded');
-        speakToUsBtn.style.display = 'inline-block';
-        searchInput.style.background = 'transparent';
-        searchInput.style.outline = 'none'
-        speakToUsBtn.style.opacity = '1';
-        glass.style.color = "black";
-    }
-}
+//header search input start// 
 
 //header search input end//
 
---
+++ src/assets/js/searchInputHome.js
@@ -0,0 +1,23 @@
+searchIcon.addEventListener('mouseenter', expandSearchInput);
+searchInput.addEventListener('mouseenter', expandSearchInput);
+function expandSearchInput() {
+    if (!searchInput.classList.contains('expanded')) {
+        searchInput.classList.add('expanded');
+        speakToUsBtn.style.opacity = '0';
+        speakToUsBtn.style.display = 'inline-block';
+        glass.style.color = "white";
+        searchInput.focus();
+    }
+}
+searchIcon.addEventListener('mouseleave', collapseSearchInput);
+searchInput.addEventListener('mouseleave', collapseSearchInput);
+function collapseSearchInput() {
+    if (searchInput.classList.contains('expanded')) {
+        searchInput.classList.remove('expanded');
+        speakToUsBtn.style.display = 'inline-block';
+        searchInput.style.background = 'transparent';
+        searchInput.style.outline = 'none'
+        speakToUsBtn.style.opacity = '1';
+        glass.style.color = "white";
+    }
+}
