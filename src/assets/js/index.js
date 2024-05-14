b8e00a58fe75c16240dc96a31c523d039628baf5
Akashm1494 <akash.m@mayaradigitale.com>
2024-05-14 19:31:51 +0530

    Add responsiveness code

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
     // jQuery code here
     // console.log("Welcome to index.js for EET");
   });
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerBtn = document.querySelector('.hamburger-btn');
+    const mediaSection = document.getElementById('media-section');
+  
+    hamburgerBtn.addEventListener('click', function () {
+      mediaSection.classList.toggle('open');
+    });
+  });
+  
