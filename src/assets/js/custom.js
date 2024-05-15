5552b37c931b51a25e635c87187addee66c0831e
sagar619619 <sc927818@gmail.com>
2024-05-15 18:09:12 +0530

    working on home page mob vie

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -15,3 +15,23 @@ function closeDropdown() {
     document.getElementById("dropdownMenu").classList.remove("show");
 }
 //header dropdown mouse hover end//
+
+//mobile collapse toggle start//
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var icon = navbarToggler.querySelector('i');
+  
+    navbarToggler.addEventListener('click', function() {
+      var expanded = navbarToggler.getAttribute('aria-expanded');
+      if (expanded === 'false' || expanded === null) {
+        // Toggle to xmark icon when navbar is expanded
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+      } else {
+        // Toggle back to bars icon when navbar is collapsed
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+      }
+    });
+  });
+//mobile collapse toggle end//
