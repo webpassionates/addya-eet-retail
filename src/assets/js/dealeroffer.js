8e06802873897366b30c233cc042187160e7a62e
sagar619619 <sc927818@gmail.com>
2024-05-15 12:28:23 +0530

    working on accordian animation

--- src/assets/js/dealeroffer.js
+++ src/assets/js/dealeroffer.js
@@ -33,10 +33,38 @@ $(document).ready(function() {
   });
   
 //accordian start//
+// $(document).ready(function(){
+//   $('.accordion__item input[type="checkbox"]').on('change', function(){
+//       // Close all other accordions
+//       $('.accordion__item input[type="checkbox"]').not(this).prop('checked', true);
+//   });
+// });
+
 $(document).ready(function(){
+  // Set initial rotation to 315 degrees for all arrows
+  $('.accordion__item input[type="checkbox"]').each(function() {
+    $(this).siblings('.border-btn').find('.fa-arrow-left').css('transform', 'rotate(315deg)');
+  });
+  // Check if the checkbox of the first accordion item is initially checked
+  var firstAccordionCheckbox = $('.accordion__item input[type="checkbox"]:first');
+  if (firstAccordionCheckbox.is(':checked')) {
+    // Rotate the left arrow icon to 360 degrees for the first accordion item when it's open
+    firstAccordionCheckbox.siblings('.border-btn').find('.fa-arrow-left').css('transform', 'rotate(45deg)');
+  }
+  // Toggle rotation and close previously opened accordion when checkbox is changed
   $('.accordion__item input[type="checkbox"]').on('change', function(){
-      // Close all other accordions
-      $('.accordion__item input[type="checkbox"]').not(this).prop('checked', true);
+    var isChecked = $(this).is(':checked');
+    // Reset rotation for all arrows
+    $('.accordion__item input[type="checkbox"]').each(function() {
+      $(this).siblings('.border-btn').find('.fa-arrow-left').css('transform', 'rotate(315deg)');
+    });
+    // Close all other accordions
+    $('.accordion__item input[type="checkbox"]').not(this).prop('checked', false);
+    // Rotate the arrow for the current accordion item
+    if (isChecked) {
+      $(this).siblings('.border-btn').find('.fa-arrow-left').css('transform', 'rotate(45deg)');
+    }
   });
 });
+
 //accordian end//
